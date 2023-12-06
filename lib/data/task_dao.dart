3b1eaaccf021feb53362dbf0144300b073d15776
fernando <fernando.oliveira@gruposc.com.br>
2023-12-06 17:29:41 -0300

    create get task dao

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,8 +1,8 @@
-
+import 'package:project1/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 import '../components/Task.dart';
 
 class TaskDao {
-
   static const String _tablename = 'taskTable';
   static const String _name = 'name';
   static const String _difficulty = 'difficulty';
@@ -13,5 +13,32 @@ class TaskDao {
       '$_difficulty INTEGER, '
       '$_photo TEXT)';
 
+  save(Task tarefa) async {}
 
+  Future<List<Task>> findAll() async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tablename);
+    return toList(result);
+  }
+
+  Future<List<Task>> find(String name) async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(
+      _tablename,
+      where: '$_name = ?',
+      whereArgs: [name],
+    );
+    return toList(result);
+  }
+
+  delete(String name) async {}
+
+  List<Task> toList(List<Map<String, dynamic>> mapTask) {
+    final List<Task> tasks = [];
+    for (Map<String, dynamic> line in mapTask) {
+      final Task task = Task(line[_name], line[_photo], line[_difficulty]);
+      tasks.add(task);
+    }
+    return tasks;
+  }
 }
