ff241654842b26f0b6ead87c3160671b578ffd74
fernando <fernando.oliveira@gruposc.com.br>
2023-12-07 18:13:02 -0300

    get list task from db

--- lib/screens/Initial_screen.dart
+++ lib/screens/Initial_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:project1/data/taskInherited.dart';
+import 'package:project1/data/task_dao.dart';
 import 'package:project1/screens/formulary.dart';
+import '../components/tarefa.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -15,10 +16,81 @@ class _InitialScreenState extends State<InitialScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Tarefas"),
+        actions: [
+          IconButton(onPressed: (){setState(() {});}, icon: const Icon(Icons.refresh))
+        ],
       ),
-      body: ListView(
+      body: Padding(
         padding: const EdgeInsets.only(top: 8, bottom: 8),
-        children: TaskInherited.of(context).taskList,
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(),
+            builder: (context, snapshot) {
+              List<Task>? items = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text(
+                          'Carregando',
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ],
+                    ),
+                  );
+                case ConnectionState.waiting:
+                  return const Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text(
+                          'Carregando',
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ],
+                    ),
+                  );
+                case ConnectionState.active:
+                  return const Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text(
+                          'Carregando',
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ],
+                    ),
+                  );
+                case ConnectionState.done:
+                  print("${snapshot.hasData}, $items");
+                  if (snapshot.hasData && items != null) {
+                    if (items.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final Task task = items[index];
+                            return task;
+                          });
+                    }
+                    return const Center(
+                      child: Column(
+                        children: [
+                          Icon(Icons.error_outline, size: 128),
+                          Text(
+                            'Nao ha nenhuma tarefa',
+                            style: TextStyle(fontSize: 32),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                  return const Text("Erro ao carregar tarefa");
+                  break;
+              }
+              return const Text('Erro desconhecido');
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -27,7 +99,7 @@ class _InitialScreenState extends State<InitialScreen> {
             MaterialPageRoute(
               builder: (contextNew) => FormularyScreen(taskContext: context),
             ),
-          );
+          ).then((value) => setState((){}));
         },
         child: const Icon(Icons.add),
       ),
